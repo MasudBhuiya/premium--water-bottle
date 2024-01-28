879e60a53b76131be737c0f2251e4033c5d4a6ad
MasudBhuiya <masudbhuiyan1415@gmail.com>
2024-01-28 23:06:15 +0600

    added our customer love section

--- src/components/Home/Home/Home.jsx
+++ src/components/Home/Home/Home.jsx
@@ -292,6 +292,79 @@ const Home = () => {
                     </div>
                 </div>
             </section>
+
+
+
+            {/* customer love section  */}
+            <div className='mt-20 md:mt-32 max-w-[1440px] mx-auto'>
+                <h1 className='title text-3xl md:text-6xl text-center max-w-[830px] mx-auto'>Our Customer Love</h1>
+                <p className=' max-w-[657px] mx-auto text-xl text-center mt-3'>Embarking on Adventures, Building Memories: Where Customer Love Takes Flight with the Travel Guru</p>
+
+
+                    {/* user details  */}
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-14 md:gap-y-20 mt-10 md:mt-20 mb-20'>
+                    <div>
+                        <p className='max-w-[350px]'>Discover how our travel guru guided this solo adventurer to off-the-beaten-path destinations, turning a solitary journey into a rich tapestry of experiences.</p>
+                        <div className='flex items-center gap-4 mt-4'>
+                            <img className='w-16 rounded-full' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcSgiG4CPYdRpP7ahhJZaDDHj5iBaOFEFDbGpvLse0trNhfr5A6efOXjA-bfjaNF1XNf0&usqp=CAU" alt="" />
+                            <div>
+                            <h1 className='font-medium '>Emily Thompson</h1>
+                        <p>UI Designer</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <p className='max-w-[350px]'>Explore the testimonials of a family who found the perfect balance of fun and relaxation, thanks to the tailored recommendations and insights provided by our travel guru.</p>
+                        <div className='flex items-center gap-4 mt-4'>
+                            <img className='w-16 rounded-full' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_pgbZQKfFX10RBv-IDDzM9Ety2UpwVp1igauCI1o63eVAOtdvPcf6Ty5XcJq78hc88dk&usqp=CAU" alt="" />
+                            <div>
+                            <h1 className='font-medium '>Carlos Rodriguez</h1>
+                        <p>UI Designer</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <p className='max-w-[350px]'>Hear from a couple who transformed their vacation into a romantic escapade, with the travel guru`s expertise leading them to the most enchanting and intimate destinations.</p>
+                        <div className='flex items-center gap-4 mt-4'>
+                            <img className='w-16 rounded-full' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnhymeWKr66OejjS-OpM8VK97n-7E5an4fe__1HaHs9MAPd6zbDfY1uwZUeGoGuUFGGR8&usqp=CAU" alt="" />
+                            <div>
+                            <h1 className='font-medium '>Sophia Johnson</h1>
+                        <p>UI Designer</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <p className='max-w-[350px]'>Delve into the story of a food enthusiast who experienced the world`s flavors with guidance from our travel guru, discovering hidden culinary gems and savoring unforgettable tastes.</p>
+                        <div className='flex items-center gap-4 mt-4'>
+                            <img className='w-16 rounded-full' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXZA75zaJus0m40dxoToryR3qD-Vo2kr1eAw&usqp=CAU" alt="" />
+                            <div>
+                            <h1 className='font-medium '>Raj Kapoor</h1>
+                        <p>UI Designer</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <p className='max-w-[350px]'>Learn how a history enthusiast immersed themselves in the rich tapestry of cultures and histories, with our travel guru`s curated itineraries providing a deep and meaningful exploration.</p>
+                        <div className='flex items-center gap-4 mt-4'>
+                            <img className='w-16 rounded-full' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY_UP9cSEjFLqwEVNDqWPhu9lUPLkzzaVpJw&usqp=CAU" alt="" />
+                            <div>
+                            <h1 className='font-medium '>Olivia Chen</h1>
+                        <p>UI Designer</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <p className='max-w-[350px]'>Follow the journey of a budget-conscious traveler who uncovered affordable yet incredible destinations and activities, proving that memorable adventures need not break the bank.</p>
+                        <div className='flex items-center gap-4 mt-4'>
+                            <img className='w-16 rounded-full' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcSgiG4CPYdRpP7ahhJZaDDHj5iBaOFEFDbGpvLse0trNhfr5A6efOXjA-bfjaNF1XNf0&usqp=CAU" alt="" />
+                            <div>
+                            <h1 className='font-medium '>Marcus Taylor</h1>
+                        <p>UI Designer</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
         </div>
     );
 };
